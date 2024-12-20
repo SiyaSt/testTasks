e643fa2ce8d11f8dd119ad7648b40b3b585442b1
oooh_shit <anastasiya.stecenko@gmail.com>
2024-12-21 01:28:33 +0300

    fix: rewrite css styles

--- todo-list/src/pages/HomePage/HomePage.jsx
+++ todo-list/src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import "./HomePage.css";
 export const HomePage = () => (
   <main className="main">
     <h3>This is homepage!</h3>
-    <p>
+    <p className="main-text">
       This app is used to make your own todo-list -
       <Link className="link" to="/todoList">
         Click to start
